009f4862cba6677201b1ab7f0ff2a548af20873d
JakubChalupnik@gmail.com <JakubChalupnik@gmail.com@c3c75b79-482b-e469-17be-15265ae610ee>
2013-08-10 18:01:52 +0000

    Added time and display support

--- ClockWork/ClockWork.ino
+++ ClockWork/ClockWork.ino
@@ -8,6 +8,7 @@
 //* Author      Date       Comment
 //*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 //* Kubik       10.8.2013 First release, testing the HW
+//* Kubik       10.8.2013 Added time and display support
 //*******************************************************************************
 
 //*******************************************************************************
@@ -21,7 +22,7 @@
 //*                           Includes and defines                              *
 //*******************************************************************************
 
-#include "TimerOne.h"
+#include "Time.h"
 
 //
 // Pins used to drive the segment chain.
@@ -37,10 +38,6 @@ const bool ShiftPWM_balanceLoad = true;
 
 #include <ShiftPWM.h>   // include ShiftPWM.h after setting the pins!
 
-//*******************************************************************************
-//*                               Static variables                              *
-//*******************************************************************************
-
 //
 // Here you set the number of brightness levels, the update frequency and the number of shift registers.
 //
@@ -49,6 +46,95 @@ const bool ShiftPWM_balanceLoad = true;
 #define ShiftPwmFrequency         75
 #define ShiftPwmRegisters         7
 
+//*******************************************************************************
+//*                               Static variables                              *
+//*******************************************************************************
+
+//*******************************************************************************
+//*                               Segment functions                             *
+//*******************************************************************************
+
+extern unsigned char SegHexTable [];
+
+void SegmentSetDigit (byte Digit, byte Brightness, byte Offset) {
+  byte i;
+  
+  for (i = 0; i < 8; i++) {
+    if (Digit & (1 << (7 - i))) {
+      ShiftPWM.SetOne (Offset, Brightness);
+    } else {
+      ShiftPWM.SetOne (Offset, 0);
+    }
+    Offset++;
+  }
+} 
+
+#define SEGMENT_BLANK   SegHexTable [0x10]
+
+void SegmentSetDisplay (byte Hour, byte Minute, int Temperature, byte Brightness) {
+  byte Digit;
+  
+  //
+  // First digit of hour could be empty for hour < 10
+  //
+  
+  if (Hour >= 10) {
+    Digit = SegHexTable [Hour / 10];
+  } else {
+    Digit = SEGMENT_BLANK;
+  }
+  
+  SegmentSetDigit (Digit, Brightness, 24);
+  
+  //
+  // Second digit of hour
+  //
+  
+  Digit = SegHexTable [Hour % 10];
+  SegmentSetDigit (Digit, Brightness, 16);
+  
+  //
+  // First digit of minute
+  //
+  
+  Digit = SegHexTable [Minute / 10];
+  SegmentSetDigit (Digit, Brightness, 8);
+  
+  //
+  // Second digit of minute
+  //
+  
+  Digit = SegHexTable [Minute % 10];
+  SegmentSetDigit (Digit, Brightness, 0);
+
+  //
+  // First digit of temperature. 
+  // Process negative temperature correctly, that is display the positive value and enable the dash
+  //
+
+  if (Temperature < 0) {
+    Temperature = -Temperature;
+    Digit = SegHexTable [Temperature / 10] | 0x01;
+  } else {
+    Digit = SegHexTable [Temperature / 10];
+  }
+
+  SegmentSetDigit (Digit, Brightness, 48);
+
+  //
+  // Second digit of temperature
+  //
+  
+  Digit = SegHexTable [Temperature % 10];
+  SegmentSetDigit (Digit, Brightness, 40);
+
+  //
+  // Degree of Celsius sign
+  //
+  
+  SegmentSetDigit (0xE0, Brightness, 32);
+}
+  
 //*******************************************************************************
 //*                            Arduino setup method                             *
 //*******************************************************************************
@@ -70,6 +156,8 @@ void setup() {
   ShiftPWM.Start (ShiftPwmFrequency, ShiftPwmMaxBrightness);
 
   ShiftPWM.SetAll (0);
+
+  setTime (0, 0, 0, 1, 1, 2013);    // Dummy time until the time gets synced 
 }
 
 //*******************************************************************************
@@ -77,7 +165,38 @@ void setup() {
 //*******************************************************************************
 
 void loop () {
-  ShiftPWM.OneByOneFast();
+  time_t Time;
+  static time_t PreviousTime = 0;
+  unsigned long Millis;
+  static unsigned long LastMillis = 0;
+
+  //
+  // Any code that needs to be executed on every loop should go here
+  //
+  
+  //
+  // Every time the Millis changes, execute the code below.
+  // In other words, all the code that follows is executed every millisecond or less frequently
+  //
+
+  Millis = millis();
+  if (Millis != LastMillis) {
+    LastMillis = Millis;
+  } else {
+    return;
+  }
+
+  //
+  // Every time the time changes, display the new time on the screen
+  // We're only displaying hours and minutes, but the code is called for every 
+  //
+
+  Time = now ();
+  if (Time != PreviousTime) {
+    PreviousTime = Time;
+    
+    SegmentSetDisplay (hour (), minute (), -32, 63);
+  }
 }
 
 
---
+++ ClockWork/LedSeg.ino
@@ -0,0 +1,34 @@
+#define _A_SEG 7
+#define _B_SEG 6
+#define _C_SEG 5
+#define _D_SEG 4
+#define _E_SEG 3
+#define _F_SEG 2
+#define _G_SEG 1
+#define _P_SEG 0
+
+#define SegMac(a,b,c,d,e,f,g,p) (((a << _A_SEG) | (b << _B_SEG) | (c << _C_SEG) | (d << _D_SEG) | (e << _E_SEG) | (f << _F_SEG) | (g << _G_SEG) | (p << _P_SEG)))
+
+//----------------------------------------------------------------------------
+// 7seg definitions
+
+unsigned char SegHexTable [] = {
+    SegMac(1, 1, 1, 1, 1, 1, 0, 0),             // 0
+    SegMac(0, 1, 1, 0, 0, 0, 0, 0),             // 1
+    SegMac(1, 1, 0, 1, 1, 0, 1, 0),             // 2
+    SegMac(1, 1, 1, 1, 0, 0, 1, 0),             // 3
+    SegMac(0, 1, 1, 0, 0, 1, 1, 0),             // 4
+    SegMac(1, 0, 1, 1, 0, 1, 1, 0),             // 5
+    SegMac(1, 0, 1, 1, 1, 1, 1, 0),             // 6
+    SegMac(1, 1, 1, 0, 0, 0, 0, 0),             // 7
+    SegMac(1, 1, 1, 1, 1, 1, 1, 0),             // 8
+    SegMac(1, 1, 1, 1, 0, 1, 1, 0),             // 9
+    SegMac(1, 1, 1, 0, 1, 1, 1, 0),             // A
+    SegMac(0, 0, 1, 1, 1, 1, 1, 0),             // B
+    SegMac(1, 0, 0, 1, 1, 1, 0, 0),             // C
+    SegMac(0, 1, 1, 1, 1, 0, 1, 0),             // D
+    SegMac(1, 0, 0, 1, 1, 1, 1, 0),             // E
+    SegMac(1, 0, 0, 0, 1, 1, 1, 0),             // F
+    SegMac(0, 0, 0, 0, 0, 0, 0, 0),             // Blank
+};
+
